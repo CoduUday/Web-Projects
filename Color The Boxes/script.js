eaf7962d2fa8a69771d39354f1557972bff7bb3d
CoduUday <udaylawand5@gmail.com>
2025-12-14 14:39:53 +0530

    Previous one was very bad :(

--- Color The Boxes/script.js
+++ Color The Boxes/script.js
@@ -1,14 +1,12 @@
-let color = {
-    "0": "green",
-    "1": "red",
-    "2": "yellow",
-    "3": "pink",
-    "4": "black",}
-let number1 = Math.floor(Math.random() * 5);
-let number2 = Math.floor(Math.random() * 5);
-
-
-let box = document.getElementsByClassName("box");
-box[number1].style.backgroundColor = `${color[number1]}`;
-box[number1].style.color = `${color[number2]}`;
-
+let boxes= document.getElementsByClassName("box")
+console.log(boxes);
+function getRandomColor() {
+    let val1=Math.floor(Math.random() * 256)
+    let val2=Math.floor(Math.random() * 256)
+    let val3=Math.floor(Math.random() * 256)
+    return `rgb(${val1}, ${val2}, ${val3})`
+}
+Array.from(boxes).forEach(e=>{
+        e.style.backgroundColor = getRandomColor()
+        e.style.color = getRandomColor()
+})
